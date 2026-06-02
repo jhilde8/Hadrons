467a2e1b4b8827d105199e7cd11b621728ab2ad0
Jonas Hildebrand <134550316+jhilde8@users.noreply.github.com>
2026-06-02 13:20:48 -0400

    Further optimization to new EMF module post testing on local machine

--- Hadrons/Modules/MContraction/A2AExtendedMesonField.hpp
+++ Hadrons/Modules/MContraction/A2AExtendedMesonField.hpp
@@ -2,7 +2,7 @@
 
 Grid physics library, www.github.com/paboyle/Grid 
 
-Source file: Hadrons/Modules/MContraction/A2AExtendedMesonField.hpp
+Grid/algorithms/multigrid/GeneralCoarsenedMatrix.hGrid/algorithms/multigrid/GeneralCoarsenedMatrix.hSource file: Hadrons/Modules/MContraction/A2AExtendedMesonField.hpp
 
 Copyright (C) 2015-2019
 
@@ -272,21 +272,28 @@ void TA2AExtendedMesonField<FImpl>::execute(void)
     Grid::A2AExtendedMesonField<FImpl>::LoopPropagator(loop, loop1, loop2);
     LOG(Message) << "Quark loop calculated" << std::endl;
 
-    std::vector<FermionField> leftv(N_i, grid);
-    for (int i = 0; i < N_i; i++)
-      Grid::A2AExtendedMesonField<FImpl>::PackLeftConjugated(leftv[i], left[i]);
-    LOG(Message) << "Memory for left vectors allocated" << std::endl;
+    //Currently trying to remove this block. No need to create so many temporaries in memory just to store the conjugate of the left vectors. 
+    //std::vector<FermionField> leftv(N_i, grid);
+    //for (int i = 0; i < N_i; i++)
+    //  Grid::A2AExtendedMesonField<FImpl>::PackLeftConjugated(leftv[i], left[i]);
+    //LOG(Message) << "Memory for left vectors allocated" << std::endl;
+
+    double t1 = 0.0;
+    double t2 = 0.0;
+    double t3 = 0.0;
+
+    std::vector<FermionField> loopRight(cacheBlock, grid);
+    PropagatorField tloop(grid);
 
     for (int &type: types_){
 
       for (int ig = 0 ; ig < gamma1_.size() ; ++ig ){
-		
+
 	A2AMatrixSet<HADRONS_A2AM_IO_TYPE> emf(mBuf.data(),1,1,nt,N_i,N_j);
-	
+
 	Vector<Gamma::Algebra> gamma1(gamma1_[ig].begin(), gamma1_[ig].end());
 	Vector<Gamma::Algebra> gamma2(gamma2_[ig].begin(), gamma2_[ig].end());
 
-	PropagatorField tloop(grid);
 	tloop = Zero();
 	switch (type) {
 	case 0: Grid::A2AExtendedMesonField<FImpl>::LoopContractionType0(tloop, loop);                 break;
@@ -295,38 +302,55 @@ void TA2AExtendedMesonField<FImpl>::execute(void)
 	case 3: Grid::A2AExtendedMesonField<FImpl>::LoopContractionType3(tloop, loop, gamma1, gamma2); break;
 	}
 	LOG(Message) << "tloop contraction done for type " << type << std::endl;
-
-	std::vector<FermionField> loopRight(N_j, grid);
-	for (int j = 0; j < N_j; j++) {
-	  switch (type) {
-	  case 0: Grid::A2AExtendedMesonField<FImpl>::LoopRightContractionType0(loopRight[j], tloop, right[j], gamma1, gamma2); break;
-	  case 1: Grid::A2AExtendedMesonField<FImpl>::LoopRightContractionType1(loopRight[j], tloop, right[j]);                 break;
-	  case 2: Grid::A2AExtendedMesonField<FImpl>::LoopRightContractionType2(loopRight[j], tloop, right[j], gamma1);         break;
-	  case 3: Grid::A2AExtendedMesonField<FImpl>::LoopRightContractionType3(loopRight[j], tloop, right[j]);                 break;
-	  }
-	}
-	LOG(Message) << "loopRight packed for type " << type << std::endl;
+	t1 = -usecond();
 	LOG(Message) << "Making EMF" << std::endl;
 	A2ASpatialSum<SpinColourVector_v> spatial_sum;
-	for ( unsigned int i = 0; i < N_i; i += cacheBlock )
+
 	for ( unsigned int j = 0; j < N_j; j += cacheBlock ){
-	  int Nii = MIN(N_i-i,cacheBlock);
 	  int Njj = MIN(N_j-j,cacheBlock);
+	  // std::vector<FermionField> loopRight(Njj, grid);
+	  t2 = -usecond();
+	  for (int jj = 0; jj < Njj; jj++) {
+	    switch (type) {
+	    case 0: Grid::A2AExtendedMesonField<FImpl>::LoopRightContractionType0(loopRight[jj], tloop, right[j+jj], gamma1, gamma2); break;
+	    case 1: Grid::A2AExtendedMesonField<FImpl>::LoopRightContractionType1(loopRight[jj], tloop, right[j+jj]);                 break;
+	    case 2: Grid::A2AExtendedMesonField<FImpl>::LoopRightContractionType2(loopRight[jj], tloop, right[j+jj], gamma1);         break;
+	    case 3: Grid::A2AExtendedMesonField<FImpl>::LoopRightContractionType3(loopRight[jj], tloop, right[j+jj]);                 break;
+	    }
+	  }
+	  t2 += usecond();
+	  LOG(Message) << "loopRight packed for block "<< j/cacheBlock << "; type " << type << "; t_LR = "<< t2 << " us" << std::endl; //outputs once per j block
+          
+	  spatial_sum.AllocateRight(Njj, grid);
+	  spatial_sum.PackRight(loopRight, 0, Njj);
 
-	  spatial_sum.Allocate(Nii, Njj, grid);
-	  spatial_sum.PackLeft (leftv,     i, Nii);
-	  spatial_sum.PackRight(loopRight, j, Njj);
+	  t3 = -usecond();
+	  for ( unsigned int i = 0; i < N_i; i += cacheBlock ) {
+	    int Nii = MIN(N_i-i,cacheBlock);
 
-	  Eigen::Tensor<ComplexD,3> emfBlock(nt, Nii, Njj);
-	  emfBlock.setZero();
-	  spatial_sum.Sum(emfBlock);
+	    // spatial_sum.Allocate(Nii, Njj, grid);
+	    // spatial_sum.PackLeftConj(left, i, Nii);
+	    // spatial_sum.PackRight(loopRight, 0, Njj);
+	    spatial_sum.AllocateLeft(Nii);
+	    spatial_sum.PackLeftConj(left, i, Nii);
 
-	  for(int t =0;t< nt;t++)
-	  for(int ii=0;ii< Nii;ii++)
-	  for(int jj=0;jj< Njj;jj++)
-	    emf(0,0,t,i+ii,j+jj) = emfBlock(t,ii,jj);
+	    Eigen::Tensor<ComplexD,3> emfBlock(nt, Nii, Njj);
+	    emfBlock.setZero();
+	    spatial_sum.Sum(emfBlock);
+
+	    for(int t =0;t< nt;t++)
+	      for(int ii=0;ii< Nii;ii++)
+		for(int jj=0;jj< Njj;jj++)
+		  emf(0,0,t,i+ii,j+jj) = emfBlock(t,ii,jj);
+	
+	    LOG(Message) << "EMF made for i-block " << i/cacheBlock << "; j-block " << j/cacheBlock << "; type " << type << std::endl; //outputs for each i-j block pair
+	}
+	t3 += usecond();
+	LOG(Message) << "EMF made for all i blocks in j block " << j/cacheBlock << "; t_block = " << t3 << " us" << std::endl;
+	
 	}// i,j
-	LOG(Message) << "Done" << std::endl;
+	t1 += usecond();
+	LOG(Message) << "EMF made, t_emf = " << t1 << " us" << std::endl; //Final output, acting as timer for full blocked sum
 
         double       blockSize, ioTime;
 
