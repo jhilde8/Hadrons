70c9175dd88cc2263219b282d1dcdc364f6ec0ce
Jonas Hildebrand <134550316+jhilde8@users.noreply.github.com>
2026-06-29 16:54:36 -0400

    remove MMF, move best case MMF to deprecated.

--- Hadrons/Modules/MContraction/A2AMomMesonField.cpp
+++
@@ -1,7 +0,0 @@
-#include <Hadrons/Modules/MContraction/A2AMomMesonField.hpp>
-
-using namespace Grid;
-using namespace Hadrons;
-using namespace MContraction;
-
-template class Grid::Hadrons::MContraction::TA2AMomMesonField<FIMPL>;
